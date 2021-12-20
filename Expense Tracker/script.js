6cad47395141c782504772c1ed4592d2ea35bd2d
payal033 <payalkarwa99@gmail.com>
2021-12-21 00:00:22 +0530

    method for updating values

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -22,7 +22,7 @@ let transactions =
           ? localStorageTransactions
           : [];
 
-
+// check income / expense type
 function checkType(value) {
   if (value === "+" && expenseBtn.classList.contains('selected')) {
     incomeBtn.classList.add("selected");
@@ -33,6 +33,7 @@ function checkType(value) {
   }
 }
 
+// add transaction
 function addTransaction(e) {
     e.preventDefault()
     if(text.value.trim() === '' || amount.value.trim() === ''){
@@ -53,32 +54,65 @@ function addTransaction(e) {
     }
 }
 
+// update local storage
 function updateLocalStorage() {
     localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
+// generate random id for transaction
 function generateID() {
     return Math.floor(Math.random() * 10000000)
 }
 
+// remove transaction from local storage
 function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id)
     updateLocalStorage()
     init()
 }
 
-function showModal() {
+var uid = 0;
+
+// to show modal and call respective methods
+function showModal(transactionID) {
   modal.classList.add('show-modal')
+  showExistingValues(transactionID)
+  uid = transactionID
+}
+
+// show existing values when clicked for updating
+function showExistingValues(id) {
+  const transaction = transactions.filter((trans) => trans.id === id)[0]
+  // console.log(transaction)
+  updatetext.value = transaction.text
+  updateamount.value = transaction.amount
+  // console.log(transaction.id)
+  updatebtn.addEventListener('click', updateTransaction)
+}
+
+// update transaction when clicked on update transaction button in modal
+function updateTransaction(e) {
+      e.preventDefault()
+      // console.log(uid)
+      const utrans = transactions.filter((trans) => trans.id === uid)[0]
+      // console.log(utrans)
+      // console.log(typeof updatetext.value, typeof +updateamount.value)
+      utrans.text = updatetext.value
+      utrans.amount = +updateamount.value
+      // console.log(utrans)
+      updateLocalStorage()
+      init()
+      modal.classList.remove('show-modal')
 }
 
 // add transactions to DOM
 function addTransactionsDOM(transaction) {
-    const sign = transaction.amount > 0 ? '+' : '-';
+    const sign = transaction.amount >= 0 ? '+' : '-';
     const item = document.createElement('li')
     // add class based on value
-    item.classList.add(transaction.amount > 0 ? 'plus' : 'minus')
+    item.classList.add(transaction.amount >= 0 ? 'plus' : 'minus')
     item.innerHTML = `
-    <div id="open" onclick="showModal()"><p>${
+    <div id="open" onclick="showModal(${transaction.id})"><p>${
       transaction.text
     }</p><span>${sign}${Math.abs(transaction.amount)}</span></div>
     <button class="delete-btn" onclick="removeTransaction(${
@@ -116,12 +150,15 @@ function init() {
 
 init()
 
+// listener to add values
 form.addEventListener('submit', addTransaction)
 
+// listener to close modal when clicked on X
 close.addEventListener("click", () => {
   modal.classList.remove("show-modal");
 });
 
+// listener to close modal when clicked outside modal
 window.addEventListener("click", (e) =>
   e.target == modal ? modal.classList.remove("show-modal") : false
 );
