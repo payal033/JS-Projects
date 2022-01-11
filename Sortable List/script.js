1b50b50e316bf8ea3668744fbcbef299b919c3db
payal033 <payalkarwa99@gmail.com>
2022-01-11 23:00:35 +0530

    updated drag and drop functionality

--- Sortable List/script.js
+++ Sortable List/script.js
@@ -2,20 +2,19 @@ const draggable_list = document.getElementById("draggable-list");
 const check_btn = document.getElementById("check");
 
 const alphabeticList = [
-  "runwalk",
-  "playsit",
-  "fastslow",
-  "happysmile",
   "facearm",
-  "lookswim",
-  "jumpjog",
-  "typewater",
+  "fastslow",
   "friendfamily",
+  "happysmile",
+  "jumpjog",
+  "lookswim",
+  "playsit",
+  "runwalk",
   "standstill",
+  "typewater",
 ];
 
 const listItems = [];
-
 let dragStartIndex;
 
 createList()
@@ -44,24 +43,45 @@ function createList() {
 }
 
 function dragStart() {
-
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
 }
 
 function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
 
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
 }
 
 function dragEnter() {
     this.classList.add('over')
-
 }
 
 function dragLeave() {
     this.classList.remove('over')
 }
 
-function dragOver() {
+function dragOver(e) {
+  e.preventDefault();
+}
 
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const word = listItem.querySelector('.draggable').innerText.trim()
+        if(word != alphabeticList[index]) {
+            listItem.classList.add('wrong')
+        }else {
+            listItem.classList.remove('wrong')
+            listItem.classList.add('right')
+        }
+    })
 }
 
 function addEventListeners() {
@@ -79,3 +99,5 @@ function addEventListeners() {
         item.addEventListener('dragleave', dragLeave)
     })
 }
+
+check_btn.addEventListener("click", checkOrder);
